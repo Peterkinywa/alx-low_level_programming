e07873cdb0701a5693fff2ca5781b35dbfcfb287
Peterkinywa <mutuapkinywa@gmail.com>
2022-03-10 11:50:16 -0800

    c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,10 @@
 #include <stdio.h>
-
 /**
-mindfull
+ *main - function
+ *Return: valve 0
  */
 int main(void)
 {
-
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
-
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
