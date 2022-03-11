76e72b4baa6b62ccd966eca507d2a35ae5d91223
Peterkinywa <mutuapkinywa@gmail.com>
2022-03-12 01:00:40 +0200

    two combinations

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,51 +1,36 @@
 #include <stdio.h>
-/* more headers goes there */
-/* betty style doc for function main goes there */
+
 /**
- * main - Entry point
- *
- * Return: Always 0 (Success)
+ * main - create 2 pairs of numbers that do not repeat
+ * Return: 0
  */
 int main(void)
 {
-	int p = 48, q = 48, y = 48, x = 49;
+	int j, i;
 
-	while (p < 58)
+	for (j = 0; j <= 99; j++)
 	{
-		while (q < 58)
+		for (i = j; i <= 99; i++)
 		{
-			while (y < 58)
+			if (i != j)
 			{
-				while (x < 58)
+
+				putchar(j / 10 + 48);
+				putchar(j % 10 + 48);
+				putchar(' ');
+				putchar(i / 10 + 48);
+				putchar(i % 10 + 48);
+
+				if (j * 100 + i != 9899)
 				{
-					putchar(p);
-					putchar(q);
+					putchar(',');
 					putchar(' ');
-					putchar(y);
-					putchar(x);
-					if (!(p == 57 &&
-						q == 56 &&
-						y == 57 &&
-						x == 57))
-					{
-						putchar(',');
-						putchar(' ');
-					}
-					x++;
 				}
-				y++;
-				x = 48;
 			}
-			q++;
-			q = p;
-			x = q + 1;
+
 		}
-		p++;
-		q = 48;
-		y = p;
-		x = q+ 1;
+
 	}
 	putchar('\n');
 	return (0);
 }
-
