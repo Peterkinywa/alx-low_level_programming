24a0e4831083358d544e1eb9697bdbe48f1b48df
Peterkinywa <mutuapkinywa@gmail.com>
2022-04-07 20:32:36 +0300

    program that multiplies two positive numbers

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -5,8 +5,6 @@
  * @str: pointer to the string to print
  * Return: void
 */
-
-
 void _puts(char *str)
 {
 int i = 0;
@@ -15,15 +13,12 @@ while (str[i])
 	_putchar(str[i]);
 	i++;
 }
-
 }
-
 /**
  * _atoi - convert a string to an integer.
  * @s: char type string
  * Return: integer converted
  */
-
 int _atoi(const char *s)
 {
     int sign = 1;
@@ -36,25 +31,20 @@ int _atoi(const char *s)
 			sign *= -1;
 		}
 	}
-
 	for (i = firstNum; s[i] >= 48 && s[i] <= 57; i++)
 	{
 		resp *= 10;
 		resp += (s[i] - 48);
 	}
-
 	return (sign * resp);
 }
-
 /**
  * print_int - prints an integer.
  * @n: int
  * Return: 0
  */
-
 void print_int(unsigned long int n)
 {
-
 unsigned  long int divisor = 1, i, resp;
 
 for (i = 0; n / divisor > 9; i++, divisor *= 10)
@@ -65,20 +55,16 @@ for (; divisor >= 1; n %= divisor, divisor /= 10)
 	resp = n / divisor;
 	_putchar('0' + resp);
 }
-
 }
-
 /**
  * main - print the result of the multiplication, followed by a new line
  * @argc: int
  * @argv: list
  * Return: 0
  */
-
 int main(int argc, char const *argv[])
 {
 (void)argc;
-
 if (argc != 3)
 {
 	_puts("Error ");
